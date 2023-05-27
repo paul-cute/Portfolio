57884a3025797783623826cdbacf0f5d6c187923
paul-cute <paulcuteflorin@protonmail.com>
2023-05-27 22:25:57 +0300

    portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className='w-full items-center py-5 fixed top-0 z-20'>
+        header
+    </nav>
+  )
+}
+
+export default Header
